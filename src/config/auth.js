22faa8803a10901dc3b03e2366e7202e8722d15a
Marcelo Leite Junior <mleitejunior@gmail.com>
2020-02-06 13:33:26 -0300

    cadastrando e editando recipient

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '4bb326378e7e968b196b54179c402213',
+  expiresIn: '7d',
+};
---
+++ src/database/migrations/20200206160540-create-recipients.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      adress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipients');
+  },
+};
